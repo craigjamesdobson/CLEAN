0c9794eada45a10ca73a2371bdbaaa79a5804097
Craig Dobson <dobson.cj@gmail.com>
2020-04-23 09:16:39 +0100

    add consistent padding

--- src/components/ChoreList.vue
+++ src/components/ChoreList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto mt-2 px-2">
-    <p v-if="filteredChores.length < 1">
+    <p class="py-2" v-if="filteredChores.length < 1">
       There are currently no chores for this day
     </p>
     <div v-else>
