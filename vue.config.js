81f3c1deaac0c5be7f0fe00ec7dc75f4a52a0ade
Craig Dobson <dobson.cj@gmail.com>
2020-04-23 09:09:47 +0100

    replace vue config with correct settings to generate manifest.json

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // ...other vue-cli plugin options...
+  pwa: {
+    name: "My App",
+    themeColor: "#FFFFFF",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black"
+  }
+};
